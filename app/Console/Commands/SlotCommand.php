f8d53cdee279acf9d1a85c4d0707e2eba2e93586
Leon Alvarez Del Canto <leon@leon-design.es>
2018-12-04 22:04:01 +0100

    update

--- app/Console/Commands/SlotCommand.php
+++ app/Console/Commands/SlotCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
+use Illuminate\Support\Collection;
 
 class SlotCommand extends Command {
 
@@ -19,14 +20,18 @@ class SlotCommand extends Command {
      *
      * @var string
      */
-    protected $description = "Give the board,paylines and win of slot game";
+    protected $description = "Slot command generate a board and the payouts for a slot game \n";
 
-    protected $bet_ammount = 100;
+    protected $bet_amount = 100;
+
     protected $cols = 5;
+
     protected $rows = 3;
+
     protected $matchs_pay_rate = [3 => 0.2 , 4 => 2, 5 => 10];
 
     protected $symbols = [9, 10, 'J', 'Q', 'K', 'A', 'C', 'D', 'M', 'B'];
+
     protected $pay_lines = [
         [0, 3, 6, 9, 12],
         [1, 4, 7, 10, 13],
@@ -41,34 +46,25 @@ class SlotCommand extends Command {
      * @return void
      */
     public function handle()
-    {   
+    {
+        $this->comment($this->description);
         $list = $this->generateList();
         $board = $this->generateBoard($list);
-        $pay_lines = $this->getPaylines($board);
-        $result = [
-            'board' => array_values($board),
+        $pay_lines = $this->getPaylinesWithWin($board);
+
+        $data = [
+            'board' => $board->values(),
             'paylines' => $pay_lines->map(function($pay_line) {
                 return (Object)[implode(' ', $pay_line['pay_line']) => $pay_line['max_matches']];
-            })->values(),
-            'bet_ammount' => $this->bet_ammount,
-            'total_win' => $pay_lines->sum('win_ammount'),
+            }),
+            'bet_amount' => $this->bet_amount,
+            'total_win' => $pay_lines->sum('win_amount'),
         ];
 
-        dump(json_encode($result));
-    }
+        $this->comment('Slot game result:' . "\n");
+        $this->info(json_encode($data));
 
-    /**
-     * Get the console command options.
-     *
-     * @return array
-     */
-    protected function getOptions()
-    {
-        return [
-            ['bet', null, InputOption::VALUE_OPTIONAL, 'The betting value for the game.', $this->bet_ammount],
-            ['symbols', null, InputOption::VALUE_OPTIONAL, 'The betting value for the game.', $this->symbols],
-            ['matchs_pay_rate', null, InputOption::VALUE_OPTIONAL, 'The betting value for the game.', $this->matchs_pay_rate],
-        ];
+        return json_encode($data);
     }
 
     /**
@@ -78,14 +74,14 @@ class SlotCommand extends Command {
      */
     protected function generateList()
     {
-        
+        $this->line("Generating items that will compound board .... . \n");
+
         $list = collect(range(0, $this->rows * $this->cols))
             ->map(function() {
                 return $this->symbols[random_int(0, count($this->symbols) - 1)];
             });
-
-        //$list = collect(['J', 'J', 'J', 'Q', 'K', 'cat', 'J', 'Q', 'monkey', 'bird', 'bird', 'bird', 'J', 'Q', 'A']);
-
+    
+        $this->info('List generated: ' . $list->implode(', ') . "\n");
         return $list;
     }
 
@@ -96,41 +92,44 @@ class SlotCommand extends Command {
      */
     protected function generateBoard($list)
     {
+        $this->line("Generating board .... . \n");
         $board = [];
         foreach ($list as $key => $symbol) {
             $row = floor($key / $this->cols);
             $col = ($key % $this->cols) * $this->rows;
             $board[$row + $col] = $symbol;
-        } 
+        }
+        $board = collect($board);
 
+        $this->info('Board generated: ' . collect($board) . "\n");
         return $board;
     }
 
     /**
-     * Get the slot paylines with ammount of matches and win ammount.
+     * Get the slot paylines with amount of matches and win amount.
      *
      * @return array
      */
-    protected function getPaylines($board)
+    protected function getPaylinesWithWin($board)
     {
         return collect($this->pay_lines)->map(function($pay_line) use ($board) {
-            $matches_and_won_ammount = $this->getMatchesAndWonAmmount($pay_line, $board);
+            $matches_and_won_amount = $this->getMatchesAndWonAmount($pay_line, $board);
             return [
                 'pay_line' => $pay_line,
-                'max_matches' => $matches_and_won_ammount['max_matches'],
-                'win_ammount' => $matches_and_won_ammount['win_ammount']
+                'max_matches' => $matches_and_won_amount['max_matches'],
+                'win_amount' => $matches_and_won_amount['win_amount']
             ];
         })->filter(function($line) {
-            return $line['max_matches'];
-        });
+            return $line['max_matches'] >= 3;
+        })->values();
     }
 
     /**
-     * Get matches and ammount won for a pay_line in a board.
+     * Get matches and amount won for a pay_line in a board.
      *
      * @return array
      */
-    protected function getMatchesAndWonAmmount($pay_line,$board) 
+    protected function getMatchesAndWonAmount($pay_line, $board) 
     {
         $line = collect($pay_line)->transform(function($item) use ($board) {
             return $board[$item];
@@ -149,9 +148,9 @@ class SlotCommand extends Command {
                 $matches[$symbol_index]++;
         }
 
-        $max_matches = max($matches) >= 3 ? max($matches) : 0 ;
-        $win_ammount = $max_matches >= 3 ? $this->matchs_pay_rate[$max_matches] * $this->bet_ammount : 0;
+        $max_matches = max($matches);
+        $win_amount = $max_matches >= 3 ? $this->matchs_pay_rate[$max_matches] * $this->bet_amount : 0;
 
-        return compact('max_matches', 'win_ammount');
+        return compact('max_matches', 'win_amount');
     }
 }
--- tests/SlotCommandTest.php
+++ tests/SlotCommandTest.php
@@ -1,20 +1,16 @@
 <?php
 
 use App\Console\Commands\SlotCommand;
+use Mockery as m;
 
 class SlotCommandTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-   /*  public function testExample()
+    public function testCommandOutput()
     {
-        $this->artisan('slot');
-    } */
+        
+    }
 
-    public function testGenerateBoardFromList()
+   /*  public function test_generate_board_from_list()
     {
         $slot_command = new SlotCommand();
         $mocked_list = collect(['J', 'J', 'J', 'Q', 'K', 'cat', 'J', 'Q', 'monkey', 'bird', 'bird', 'bird', 'J', 'Q', 'A']);
@@ -29,7 +25,7 @@ class SlotCommandTest extends TestCase
         $this->assertEmpty($expected_board->diffAssoc($board));
     }
 
-    public function testGetMatchesAndWonAmmount()
+    public function test_payline_with_3_matches()
     {
         $slot_command = new SlotCommand();
         $mocked_payline = collect([0, 3, 6, 9, 12]);
@@ -39,15 +35,94 @@ class SlotCommandTest extends TestCase
             2 => 'bird', 5 => 'bird', 8 => 'J', 11 => 'Q', 14 => 'A'
         ]);
 
-        $matches_and_won = $this->invokeMethod($slot_command, 'getMatchesAndWonAmmount', array($mocked_payline, $mocked_board));
-        $expected_matches = collect(['max_matches' => 3, 'win_ammount' => 20]);
+        $matches_and_won = $this->invokeMethod($slot_command, 'getMatchesAndWonAmount', array($mocked_payline, $mocked_board));
+        $expected_matches = collect(['max_matches' => 3, 'win_amount' => 20]);
+
         $this->assertEmpty($expected_matches->diffAssoc($matches_and_won));
     }
 
+    public function test_payline_with_4_matches()
+    {
+        $slot_command = new SlotCommand();
+        $mocked_payline = collect([2, 5, 8, 11, 14]);
+        $mocked_board = collect([
+            0 => 'J', 3 => 'J', 6 => 'J', 9 => 'Q', 12 => 'K',
+            1 => 'cat', 4 => 'J', 7 => 'Q', 10 => 'monkey', 13 => 'bird',
+            2 => 'bird', 5 => 'bird', 8 => 'bird', 11 => 'bird', 14 => 'K'
+        ]);
 
+        $matches_and_won = $this->invokeMethod($slot_command, 'getMatchesAndWonAmount', array($mocked_payline, $mocked_board));
+        $expected_matches = collect(['max_matches' => 4, 'win_amount' => 200]);
 
+        $this->assertEmpty($expected_matches->diffAssoc($matches_and_won));
+    }
+
+    public function test_payline_with_5_matches()
+    {
+        $slot_command = new SlotCommand();
+        $mocked_payline = collect([2, 5, 8, 11, 14]);
+        $mocked_board = collect([
+            0 => 'J', 3 => 'J', 6 => 'J', 9 => 'Q', 12 => 'K',
+            1 => 'cat', 4 => 'J', 7 => 'Q', 10 => 'monkey', 13 => 'bird',
+            2 => 'monkey', 5 => 'monkey', 8 => 'monkey', 11 => 'monkey', 14 => 'monkey'
+        ]);
+
+        $matches_and_won = $this->invokeMethod($slot_command, 'getMatchesAndWonAmount', array($mocked_payline, $mocked_board));
+        $expected_matches = collect(['max_matches' => 5, 'win_amount' => 1000]);
+
+        $this->assertEmpty($expected_matches->diffAssoc($matches_and_won));
+    }
+
+    public function test_payline_with_no_matches()
+    {
+        $slot_command = new SlotCommand();
+        $mocked_payline = collect([1, 4, 7, 10, 13]);
+        $mocked_board = collect([
+            0 => 'J', 3 => 'J', 6 => 'J', 9 => 'Q', 12 => 'K',
+            1 => 'cat', 4 => 'J', 7 => 'Q', 10 => 'monkey', 13 => 'bird',
+            2 => 'bird', 5 => 'bird', 8 => 'J', 11 => 'Q', 14 => 'A'
+        ]);
+
+        $matches_and_won = $this->invokeMethod($slot_command, 'getMatchesAndWonAmount', array($mocked_payline, $mocked_board));
+        $expected_matches = collect(['max_matches' => 1, 'win_amount' => 0]);
+
+        $this->assertEmpty($expected_matches->diffAssoc($matches_and_won));
+    }
+
+    public function test_get_board_paylines_with_win() {
+        $slot_command = new SlotCommand();
+        $mocked_board = collect([
+            0 => 'J', 3 => 'J', 6 => 'J', 9 => 'Q', 12 => 'K',
+            1 => 'cat', 4 => 'J', 7 => 'Q', 10 => 'monkey', 13 => 'bird',
+            2 => 'bird', 5 => 'bird', 8 => 'J', 11 => 'Q', 14 => 'A'
+        ]);
+
+        $paylines_with_win = $this->invokeMethod($slot_command, 'getPaylinesWithWin', array($mocked_board));
+        
+        $expected_paylines_with_win = collect([
+            [
+                "pay_line" => [0, 3, 6, 9, 12],
+                "max_matches"  => 3,
+                "win_amount"  => 20.0
+            ],
+            [
+                "pay_line" => [0, 4, 8, 10, 12],
+                "max_matches" => 3,
+                "win_amount" => 20.0
+            ],
+        ]);
+
+        $this->assertEmpty($expected_paylines_with_win->filter(function ($payline, $index) use ($paylines_with_win) {
+            return collect($payline)
+                ->flatten()
+                ->diffAssoc(collect($paylines_with_win[$index])->flatten())
+                ->count();
+        }));
+
+    }
+ */
     /**
-     * Call protected/private method of a class.
+     * Call protected/private method of a class to allow testing it.
      *
      * @param object &$object    Instantiated object that we will run method on.
      * @param string $methodName Method name to call
